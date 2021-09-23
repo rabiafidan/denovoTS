25e0c2da92debc9937fafeb365b6ffb234c68547
fidan <f.rabiafidan@gmail.com>
2021-09-23 21:24:58 +0100

    added singularity because of DeepTrio

--- scripts/snakemake.sh
+++ scripts/snakemake.sh
@@ -7,9 +7,12 @@
 #BSUB -J snakemake
 #BSUB -M 2048
 
+module purge
+module load singularity-3.7.0-gcc-9.3.0-dp5ffrp
+module load python-3.9.5-gcc-9.3.0-jtayjft
 
 conda init
 . ~/.bashrc
 
 conda activate Rabia
-snakemake --cluster "bsub -M {resources.mem_mb} -n {threads} -q standard -J snakemake -e {params.err} -o {params.out}" --jobs 200 --keep-going
+snakemake --rerun-incomplete --cluster "bsub -M {resources.mem_mb} -n {threads} -q standard -J snakemake -e {params.err} -o {params.out}" --jobs 200 --keep-going --use-singularity
